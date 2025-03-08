4a68b361f6b53b6f57f00e0f8eb725276ff9065b
Cindy Clara <dev.satrio.ap@gmail.com>
2025-03-08 10:13:39 +0700

    password input as reusable component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { useState } from "react";
 import { Card, CardBody } from "@heroui/card";
 import { Tab, Tabs } from "@heroui/tabs";
 import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 import { Link } from "@heroui/link";
 import { Form } from "@heroui/form";
-import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+
+import { PasswordInput } from "@/components/password-input";
 
 export default function LoginPage() {
   return (
@@ -46,36 +46,3 @@ export default function LoginPage() {
   );
 }
 
-const PasswordInput = ({
-  label,
-  variant = "flat"
-}: {
-  label: string;
-  variant?: "flat" | "bordered" | "faded" | "underlined";
-}) => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisibility = () => setIsVisible(!isVisible);
-
-  return (
-    <Input
-      label={label}
-      type={isVisible ? "text" : "password"}
-      variant={variant}
-      endContent={
-        <button
-          aria-label="toggle password visibility"
-          className="focus:outline-none"
-          type="button"
-          onClick={toggleVisibility}
-        >
-          {isVisible ? (
-            <EyeSlashIcon className="w-6 h-6 text-default-400 pointer-events-none" />
-          ) : (
-            <EyeIcon className="w-6 h-6 text-default-400 pointer-events-none" />
-          )}
-        </button>
-      }
-    />
-  );
-};
---
+++ components/password-input.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Input } from "@heroui/input";
+
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+
+interface PasswordInputProps {
+    label: string;
+    variant?: "flat" | "bordered" | "faded" | "underlined";
+};
+
+export const PasswordInput = ({
+    label,
+    variant = "flat"
+  }: PasswordInputProps) => {
+    const [isVisible, setIsVisible] = useState(false);
+  
+    const toggleVisibility = () => setIsVisible(!isVisible);
+  
+    return (
+      <Input
+        label={label}
+        type={isVisible ? "text" : "password"}
+        variant={variant}
+        endContent={
+          <button
+            aria-label="toggle password visibility"
+            className="focus:outline-none"
+            type="button"
+            onClick={toggleVisibility}
+          >
+            {isVisible ? (
+              <EyeSlashIcon className="w-6 h-6 text-default-400 pointer-events-none" />
+            ) : (
+              <EyeIcon className="w-6 h-6 text-default-400 pointer-events-none" />
+            )}
+          </button>
+        }
+      />
+    );
+  };
+  
