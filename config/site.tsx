1aaa3dc9f7ce9f3f55646c380847ae6cbe8f7e40
Cindy Clara <dev.satrio.ap@gmail.com>
2025-03-11 00:39:13 +0700

    refactor site config

--- config/site.tsx
+++ config/site.tsx
@@ -20,6 +20,48 @@ const sidebarTheme = {
   linkIndicator: "#ffffff",
 };
 
+const sidebarMenuItems = [
+  {
+    label: "Ikhtisar",
+    href: "/",
+    Icon: (props: IconSvgProps) => (
+      <DashboardIcon {...props} height={25} width={25} />
+    ),
+  },
+  {
+    label: "Laporan",
+    href: "/reports",
+    Icon: (props: IconSvgProps) => (
+      <DocumentTextIcon className={iconClassname} {...props} />
+    ),
+  },
+  {
+    label: "Statistik",
+    href: "/statistics",
+    Icon: (props: IconSvgProps) => (
+      <ChartBarSquareIcon className={iconClassname} {...props} />
+    ),
+  },
+  {
+    label: "Pengumuman",
+    href: "/announcements",
+    Icon: (props: IconSvgProps) => (
+      <MegaphoneIcon className={iconClassname} {...props} />
+    ),
+  },
+];
+
+const additionalMenuItems = [
+  {
+    label: "Pengaturan",
+    href: "/settings",
+  },
+  {
+    label: "Keluar",
+    href: "/logout",
+  },
+];
+
 export const siteConfig = {
   name: "E-Lapor Dashboard",
   description: "Dashboard Pelaporan Elektronik",
@@ -30,46 +72,6 @@ export const siteConfig = {
     "https://chnpxcvhzxlwdaqhbhqp.supabase.co/storage/v1/object/sign/photos/Firefly%20generate%20image%20on%20indonesian%20landscape%200%20(3).jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwaG90b3MvRmlyZWZseSBnZW5lcmF0ZSBpbWFnZSBvbiBpbmRvbmVzaWFuIGxhbmRzY2FwZSAwICgzKS5qcGciLCJpYXQiOjE3NDE2MjgxMDUsImV4cCI6MTgwNDcwMDEwNX0.JMEF7P6GMMmLwABZ5Yzqh4weGX6TB7w6Fbbhd_G0330",
   ],
   sidebarTheme,
-  menuItems: [
-    {
-      label: "Ikhtisar",
-      href: "/",
-      Icon: (props: IconSvgProps) => (
-        <DashboardIcon {...props} height={25} width={25} />
-      ),
-    },
-    {
-      label: "Laporan",
-      href: "/reports",
-      Icon: (props: IconSvgProps) => (
-        <DocumentTextIcon className={iconClassname} {...props} />
-      ),
-    },
-    {
-      label: "Statistik",
-      href: "/statistics",
-      Icon: (props: IconSvgProps) => (
-        <ChartBarSquareIcon className={iconClassname} {...props} />
-      ),
-    },
-    {
-      label: "Pengumuman",
-      href: "/announcements",
-      Icon: (props: IconSvgProps) => (
-        <MegaphoneIcon className={iconClassname} {...props} />
-      ),
-    },
-  ],
-  settingsItems: [
-    {
-      label: "Pengaturan",
-      href: "/settings",
-      icon: <Cog6ToothIcon className={iconClassname} />,
-    },
-    {
-      label: "Keluar",
-      href: "/logout",
-      icon: <ArrowLeftStartOnRectangleIcon className={iconClassname} />,
-    },
-  ],
+  menuItems: sidebarMenuItems,
+  settingsItems: additionalMenuItems,
 };
