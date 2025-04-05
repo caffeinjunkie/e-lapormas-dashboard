38ddbdb5eea8fd8bf58dae562921a7afcf00e9a6
Cindy Clara <dev.satrio.ap@gmail.com>
2025-04-05 12:43:53 +0700

    use kb on below 500kb image size

--- utils/string/index.ts
+++ utils/string/index.ts
@@ -124,8 +124,8 @@ const formatLocaleDate = (
 };
 
 const getByteSize = (size: number) => {
-  if (size < 1048576) {
-    return (size / 1024).toFixed(2) + "KB";
+  if (size < 500000) {
+    return (size / 1024).toFixed(0) + "KB";
   }
   return (size / 1048576).toFixed(2) + "MB";
 };
