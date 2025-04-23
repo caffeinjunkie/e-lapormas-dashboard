c3bb90b94cec0ea7d5e0bbace59a67d4091c9f94
caffein.junkie <dev.satrio.ap@gmail.com>
2025-04-23 21:18:52 +0700

    private env key

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  env: {
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+    PEKERJA_AI_IMG_UPLOAD_KEY: process.env.PEKERJA_AI_IMG_UPLOAD_KEY,
+  },
   images: {
     remotePatterns: [
       {
