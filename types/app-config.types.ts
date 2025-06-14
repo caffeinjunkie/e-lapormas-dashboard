bb823f1d5bc260259dbf9fc2853b0868b6478150
Satrio <104870883+caffeinjunkie@users.noreply.github.com>
2025-06-14 22:33:58 +0700

    feat: add admin limit (#36)
    * disable buttons and switch when hit admins limit
    
    * super admin icon

--- types/app-config.types.ts
+++ types/app-config.types.ts
@@ -1,5 +1,12 @@
+type Subscription = {
+  plan?: string;
+  super_admins?: boolean;
+  admins?: number;
+};
+
 export type AppConfig = {
   id?: number;
   org_name?: string;
   timezone?: string;
+  subscription?: Subscription;
 };
